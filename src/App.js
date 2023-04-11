a9bdf618733299fb453092b3c07cafa651fe88ea
duongthaidat175@gmail.com <duongthaidat175@gmail.com>
2023-04-11 11:50:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { nanoid } from 'nanoid'
 import NotesList from './component/NotesList';
+import Search from './component/Search';
+import Header from './component/Header';
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -24,6 +26,23 @@ const App = () => {
 
 
   ]);
+
+  const [searchText, setSearchText] = useState('')
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    const saveNote = JSON.parse(
+      localStorage.getItem('react-note-app-data'))
+      if (saveNote){
+        setNotes(saveNote)
+      }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('react-notes-app-data',
+      JSON.stringify(notes))
+  }, [notes])
+
   const addNote = (text) => {
     const date = new Date()
     const newNote = {
@@ -34,11 +53,24 @@ const App = () => {
     const newNotes = [...notes, newNote]
     setNotes(newNotes)
   }
-  return <div className='container'>
-    <NotesList
-      notes={notes}
-      handleAddNote={addNote}
-      {console.log(handleAddNote)} />
-  </div>
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id)
+    setNotes(newNotes)
+  }
+  return (
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className='container'>
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
+          )}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
+    </div>
+  )
 }
 export default App;
--- src/component/AddNote.js
+++ src/component/AddNote.js
@@ -3,13 +3,17 @@ import React, { useState } from "react"
 
 const AddNote = ({ handleAddNote }) => {
     const [noteText, setNoteText] = useState('')
-
+    const characterLimit = 200
     const handleChange = (event) => {
-        setNoteText(event.target.value);
+        if (characterLimit - event.target.value.length >= 0) {
+            setNoteText(event.target.value);
+        }
     }
     const handleSaveClick = () => {
-        console.log(handleAddNote);
-        // handleAddNote(noteText)
+        if (noteText.trim().length > 0) {
+            handleAddNote(noteText)
+            setNoteText('')
+        }
     }
     return (<div className="note new">
         <textarea
@@ -20,7 +24,7 @@ const AddNote = ({ handleAddNote }) => {
             onChange={handleChange}
         ></textarea>
         <div className="note-footer">
-            <small> 200 Remaining</small>
+            <small> {characterLimit - noteText.length} Remaining</small>
             <button className="save" onClick={handleSaveClick}>Save</button>
         </div>
     </div>)
